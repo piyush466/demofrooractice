3e9649208df3bc56b93b08f2bcbd5f4a6e5517ee
piyushdravyakar46 <piyushdravyakar46@gmail.com>
2023-05-03 12:10:11 +0530

    done 1

--- handle.py
+++ handle.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -52,13 +53,80 @@ time.sleep(2)
 # # tet=driver.find_element(By.XPATH,'//*[@id="header-part"]/div[2]/div/div/div[2]/div/div[1]/div[2]/span').text
 # # print(tet)
 
+# driver.find_element(By.CSS_SELECTOR, "#openwindow").click()
+#
+# print(driver.current_url)
+# test = driver.window_handles
+# print(test)
+# new_wind = driver.switch_to.window(test[1])
+# print(driver.current_url)
+# test = driver.find_element(By.XPATH,"/html/body/header/div[1]/div/div/div[1]/div/ul/li[1]/span")
+# print(test.text)
+# test0 = driver.window_handles
+# driver.switch_to.window(test0[0])
+# print(driver.current_url)
+# print(driver.title)
 
-driver.find_element(By.CSS_SELECTOR, "#openwindow").click()
-print(driver.current_url)
-test = driver.window_handles
-print(test)
-new_wind = driver.switch_to.window(test[1])
-print(driver.current_url)
-test = driver.find_element(By.XPATH,"/html/body/header/div[1]/div/div/div[1]/div/ul/li[1]/span")
-print(test.text)
+#
+# test = driver.window_handles
+# print(test)
+# time
+# driver.find_element(By.CSS_SELECTOR, "#openwindow").click()
+# time.sleep(5)
+# id= driver.current_window_handle
+# print(id)
+# # if id in test:
+# #     driver.switch_to.window(id)
+# #     test = driver.find_element(By.XPATH,"/html/body/header/div[1]/div/div/div[1]/div/ul/li[1]/span")
+# #     print(test.text)
+#
+# time.sleep(5)
+
+# driver.find_element(By.ID,'name').send_keys(" piyush tester ")
+# driver.find_element(By.ID,'alertbtn').click()
+#
+#
+# test=driver.switch_to.alert
+#
+# print(test.text)
+#
+# time.sleep(5)
+
+
+# locte= driver.find_element(By.ID,'mousehover')
+# print(locte.location)
+#
+# driver.execute_script("window.scrollBy(23,1374)")
+#
+#
+# act= ActionChains(driver)
+# act.move_to_element(locte).perform()
+#
+# time.sleep(2)
+#
+# driver.find_element(By.LINK_TEXT,'Top').click()
+# time.sleep(5)
+
+
+
+driver.find_element(By.ID,'autocomplete').send_keys('in')
+time.sleep(5)
+
+a=True
+
+# while a:
+ # for name in country_name:
+country_name = driver.find_elements(By.XPATH, '//*[@id="ui-id-1"]/li')
+for name in country_name:
+
+    print(name.text)
+    if name.text.endswith('dia'):
+        print(name.text)
+        name.click()
+        break
+    # a=False
+
+time.sleep(10)
+
+
 
